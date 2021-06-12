9694f69766d9c34e1419633e620fafc59bbac19a
ahnella04 <chqkq96@gmail.com>
2021-06-12 22:33:56 +0900

    [Update]ProductsList

--- TechMarket-Client/techmarket/src/pages/ProductsPage.js
+++ TechMarket-Client/techmarket/src/pages/ProductsPage.js
@@ -1,8 +1,6 @@
-
 import React, { useState } from "react";
 import Nav from "../components/Nav";
 import { Link } from "react-router-dom";
-
 
 import feature from "../images/Earl of Lemongrab.jpeg";
 
@@ -12,22 +10,18 @@ import "../styles/ProductsPage.css";
 import img from "../images/profileImg.png";
 
 const Products = () => {
-
   const [item, setItem] = useState(0);
   const incrementItem = () => setItem(item + 1);
 
   const [products, setProducts] = useState([]);
 
-  axios.get(
-    `http://localhost:8080/products`
-  )
-  .then(res => {
-    if(res.status === 200) {
+  axios.get(`http://localhost:8080/products`).then(res => {
+    if (res.status === 200) {
       setProducts(res.data);
     } else {
       alert("예상치 못한 오류가 발생했습니다. \n 잠시 후 다시 시도해주세요.");
     }
-  })
+  });
 
   return (
     <>
@@ -43,28 +37,24 @@ const Products = () => {
             </div>
           </div>
 
-            {
-              products.map(product => {
-                return (
-                  <div className="body-products" key={product.id}>
-                    <img src={img}></img>
-                    <div className="product-title">{product.title}</div>
-                    <div className="product-category">{product.category}</div>
-                    <div className="product-writer-container">
-                      <span className="product-writer">{product.writerid}</span>
-                      <span className="product-users-watch">
-                        {/* 하트 이모지*/}32
-                      </span>
-                    </div>
-                  </div>    
-              )})
-            }  
-
-          </div>
-
+          {products.map(product => {
+            return (
+              <div className="body-products" key={product.id}>
+                <img src={img}></img>
+                <div className="product-title">{product.title}</div>
+                <div className="product-category">{product.category}</div>
+                <div className="product-writer-container">
+                  <span className="product-writer">{product.writerid}</span>
+                  <span className="product-users-watch">
+                    {/* 하트 이모지*/}32
+                  </span>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div id="products-footer"></div>
       </div>
+      <div id="products-footer"></div>
     </>
   );
 };
